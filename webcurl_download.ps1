ba9f8e31c9dc3ad3013af74ca26d5f448a1bd0f1
jaycgit <jay.crispo@gmail.com>
2018-04-09 22:33:40 -0400

    Add files via upload

---
+++ webcurl_download.ps1
@@ -0,0 +1,5 @@
+﻿$day=(Get-date).ToString("MMddyyyy.'csv'")
+D:
+cd D:\ServerEPS_active
+del Export TRX01*.csv
+curl -k "https://www.sitename.com/servervice/export/alltransactions.xpt?companynumber=XXXX&username=xxxxxxxx&password=xxxxxx&format=CSV" > "Export TRX01 $day"
